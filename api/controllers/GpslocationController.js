94a82185d220caf1979d7932be5ed13972ab3445
Okan Kurtulus <okankurtulus@gmail.com>
2016-09-20 14:43:48 +0300

    - Added api functions

---
+++ api/controllers/GpslocationController.js
@@ -0,0 +1,11 @@
+/**
+ * GpslocationController
+ *
+ * @description :: Server-side logic for managing gpslocations
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
